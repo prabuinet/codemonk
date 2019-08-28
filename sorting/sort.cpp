52b8940c4bf16b3b2514c491d87bef60c89b1fb9
Prabu Natarajan <prabuinet@gmail.com>
2019-08-28 23:34:04 +0530

    insert bubble selection

---
+++ sorting/sort.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+using namespace std;
+
+/* 32k ints sorted in 3 seconds */
+
+// keeps swapping each element from 0 to n-1, so that highest reaches n-1
+// in next iteration does the same from 0 to n-2, so the next highest reaches n-2
+// ...
+int bubble(int arr[], int n) {
+
+  for(int i = n - 1; i >= 1; i--) {
+
+    for(int j = 0; j < i; j++) {
+
+      if(arr[j] > arr[j + 1]) {
+	int temp = arr[j];
+	arr[j] = arr[j + 1];
+	arr[j + 1] = temp;
+      }
+      
+    }
+    
+  }
+
+}
+
+/* 32k ints sorted in 1.5 seconds */
+
+// find the min element of arr[0..n-1] and put it in the a[0]
+// find the min element of arr[1..n-1] and put it in the a[1]
+// ...
+int selection(int arr[], int n) {
+
+  for(int i = 0; i < n-1; i++) {
+    
+    int min = i;
+
+    for(int j = i + 1; j < n; j++) {
+
+      if (arr[j] < arr[min])
+	min = j;
+      
+    }
+
+    int temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+            
+  }
+}
+
+// insertion sort works like its working on an already sorted array
+// consider for 5th element it checks the element agaist 
+int insertion(int arr[], int n){
+
+  for(int i = 0; i < n; i++) {
+
+    int temp = arr[i];
+    int j;
+    
+    for(j = i; j > 0 && arr[j-1] > temp; j--)
+      arr[j] = arr[j-1];
+    
+
+    arr[j] = temp;
+  }
+  
+}
+
+
+
+int xmain() {
+
+  int arr[] = { 5, 4, 5, 2, 50, 3, 10 };
+
+  //bubble(arr, 7);
+  //selection(arr, 7);
+  //insertion(arr, 7);
+
+  for(int i = 0; i < 7; i++)
+    cout << arr[i] << ' ';
+
+  cout << '\n';
+}
+
+int main() {
+  int arr_size = 100000;
+  
+  int arr[arr_size];
+
+  int n = 0;
+
+  int x;
+
+  while(cin >> x) {
+    arr[n++] = x;
+  }
+
+  //bubble(arr, n);
+  //selection(arr, n);
+  //insertion(arr, n);
+
+  for(int i = 0; i < n; i++)
+    cout << arr[i] << endl;  
+  
+  return 0;
+}
